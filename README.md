3ed6e1b23abc4fbac3d8ade97b733d44ab3c8cb7
秋雨落 <i@rain.cx>
2021-05-03 19:36:07 +0800

    :tada: Init
    Signed-off-by: 秋雨落 <i@rain.cx>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# PastryMaster
+# PastryMaster / 面点大师
 Be a pastry master. 快来一起成为面点大师吧。
+
+在Bukkit服务器中揉揉你的好朋友。
+
+![什么是面点大师](https://github.com/AmemiyaSigure/PastryMaster/raw/main/img/1.jpg)
---
+++ src/main/java/cx/rain/mc/pastrymaster/PastryMaster.java
@@ -0,0 +1,20 @@
+package cx.rain.mc.pastrymaster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PastryMaster extends JavaPlugin {
+    public PastryMaster() {
+
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
